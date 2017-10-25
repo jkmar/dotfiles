8146b24a1ab927841255caf1dac51b293921c8e3
jkmar <marciniszyn.jk@gmail.com>
2017-10-25 16:51:51 +0200

    History configuration

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ BACKUP=".backup"
 cd "${DIR}"
 
 mkdir ${BACKUP}
-mv $HOME/.vimrc ${BACKUP}/
-mv $HOME/.tmux.conf ${BACKUP}/
-mv $HOME/.bashrc ${BACKUP}/
-mv $HOME/.inputrc ${BACKUP}/
-mv $HOME/.pythonrc ${BACKUP}/
+mv ${HOME}/.vimrc ${BACKUP}/
+mv ${HOME}/.tmux.conf ${BACKUP}/
+mv ${HOME}/.bashrc ${BACKUP}/
+mv ${HOME}/.inputrc ${BACKUP}/
+mv ${HOME}/.pythonrc ${BACKUP}/
 
 echo "so ${PWD}/vim/vimrc.vim" > ${HOME}/.vimrc
 echo "source ${PWD}/tmux/tmux.conf" > ${HOME}/.tmux.conf
@@ -20,3 +20,5 @@ echo "source ${PWD}/bash/bashrc" > ${HOME}/.bashrc
 
 cp pythonrc ${HOME}/.pythonrc
 cp inputrc ${HOME}/.inputrc
+
+cat ${HOME}/.bash_history >> ${HOME}/.bash_eternal_history
